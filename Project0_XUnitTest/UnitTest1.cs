55e1dd517c699718dbae34d4675aab86185f4017
Mark Moore <mark.c.moore@hotmail.com>
2019-10-22 09:04:44 -0500

    Completed test suite

--- Project0_XUnitTest/UnitTest1.cs
+++ Project0_XUnitTest/UnitTest1.cs
@@ -30,6 +30,15 @@ namespace Project0_XUnitTest
 
         /*************************************************************************/
 
+        private Customer customer;
+        private Customers customers;
+        private Location location;
+        private Locations locations;
+        private Product product;
+        private Products products;
+        private Order order;
+        private Orders orders;
+
         [Fact]//make sure product constructor works.
         public void Test1()
         {
@@ -40,12 +49,39 @@ namespace Project0_XUnitTest
             var test = product0.ProductName;
 
             //assert
-            Assert.Equal(expected: test, actual: "bamboo");
+            Assert.Equal(expected: "bamboo" , actual: test);
         }
 
+        //make sure 
+        public void Test2()
+        {
+            //arrange
+            int fivefive = 55;
+
+            //act
+            customer.CustID = fivefive;
+
+            //assert
+            Assert.Equal(expected: 55, actual: customer.CustID);
+
+
+        }
+        //make sure defualt values are set
+        public void Test3()
+        {
+            //arrange
+            Customer testCust = new Customer(101, "fName", "lName");
+
+            //act
+
+
+            //assert
+            Assert.Equal(expected: 00000, actual: testCust.CustomerZipCode);
+
+        }
 
         [Fact]//make sure product constructor works.
-        public void Test2()
+        public void Test4()
         {
             //arrange
             Product product0 = new Product("apples", 10);
@@ -54,26 +90,24 @@ namespace Project0_XUnitTest
             var test = product0.ProductPrice;
 
             //assert
-            Assert.Equal(expected: test, actual: 10);
+            Assert.Equal(expected: 10, actual: test);
         }
-/*
-        [Fact]//make sure add product works
-        public void Test9()
+
+        [Fact]//make sure  product constructor works
+        public void Test5()
         {
+            //arrange
             Product product0 = new Product("bana-na-na", 10);
 
             //act
-            DBRepository.AddProduct(product0);
-
-
 
             //assert
-            Assert.Equal(expected: testProducts.Count, actual: 3);
-        }*/
+            Assert.Equal(expected: "bana-na-na", actual: product0.ProductName);
+        }
 
 
         [Fact]//make sure Location constructor works.
-        public void Test3()
+        public void Test6()
         {
             //arrange
             Location loc= new Location("London");
@@ -82,35 +116,26 @@ namespace Project0_XUnitTest
             var test = loc.LocationName;
 
             //assert
-            Assert.Equal(expected: test, actual: "London");
+            Assert.Equal(expected: "London", actual: test);
         }
 
         [Fact]//make sure Order constructor works.
-        public void Test4()
+        public void Test7()
         {
             //arrange
             Order ord = new Order();
 
             //act
-            //DBRepository.AddOrder(ord);
+            ord.LocationID = 321;
 
             //assert
-            //Assert.Equal(expected: DbLibrary.customers.Count, actual: 3);
+            Assert.Equal(expected: 321, actual: ord.LocationID);
 
         }
 
-        /*            testCustomers = DbLibrary.ReadAllCustomers();
-                    Console.WriteLine(testCustomers.Count);
-                    foreach (Customer x in testCustomers)
-                    {
-                        Console.WriteLine($"{x.CustID}, {x.CustomerFirstName}, {x.CustomerLastName}, {x.CustomerStreet}, {x.CustomerCity}, {x.CustomerZipCode}");
-                        Console.WriteLine();
-                    }*/
 
-
-
-        [Fact]
-        public void Test5()
+        [Fact]//test Location constructor with all params provided
+        public void Test8()
         {
             //arrange
             Location Location1 = new Location("Fort Worth", "432 MIllbrook Ln.", "For Worth", 44444);
@@ -119,63 +144,74 @@ namespace Project0_XUnitTest
             var test = Location1.LocationCity;
 
             //assert
-            Assert.Equal(expected: test, actual: "Fort Worth");
+            Assert.Equal(expected: "Fort Worth", actual: test);
         }
 
 
 
 
         [Fact]
-        public void Test6()
+        public void Test9()
         {
             //arrange
-            Location Location1 = new Location("Fort Worth", "432 MIllbrook Ln.", "For Worth", 44444);
-            Location Location2 = new Location("Dallas", "1 MIllbrook Ln.", "Dallas", 55555);
-            Location Location3 = new Location("Burleson", "2 MIllbrook Ln.", "Burleson", 66666);
-
-    /*        //act
-            DbLibrary.AddLocation(Location1);
-            DbLibrary.AddLocation(Location2);
-            DbLibrary.AddLocation(Location3);
-
-            //assert
-            testLocations = DbLibrary.ReadAllLocations();
-            Assert.Equal(expected: DbLibrary.locations.Count, actual: 3);*/
-
-        }
-
-        [Fact]
-        public void Test7()
-        {
-            //arrange
-            //Order order1 = new Order(11, "Mary 27, 1979", 22);
+            Order order = new Order();
 
             //act
-            //var test = order1.OrderDate;
+            order.itemsOrdered.Add("jam", 7);
+            order.itemsOrdered.Add("jem", 8);
+            order.itemsOrdered.Add("jim", 9);
+
 
             //assert
-            //Assert.Equal(expected: test, actual: "Mary 27, 1979");
+            Assert.Equal(expected: 3, actual: order.itemsOrdered.Count);
+
+        }
+
+        [Fact]
+        public void Test10()
+        {
+            //arrange
+            Order order = new Order();
+
+            //act
+            order.itemsOrdered.Add("jam", 7);
+            order.itemsOrdered.Add("jem", 8);
+            order.itemsOrdered.Add("jim", 9);
+
+            //assert
+            Assert.Equal(expected: 7, actual: order.itemsOrdered["jam"]);
         }
 
 
-
-
-        [Fact]
-        public void Test8()
+        [Fact]//verify overwritting of values in Order dictionary
+        public void Test11()
         {
             //arrange
-           // Order order1 = new Order(11, "May 27, 1979", 22);
-
-
+            Order order = new Order();
 
             //act
-           // DbLibrary.AddOrder(order1);
-
+            order.itemsOrdered.Add("jam", 7);
+            order.itemsOrdered.Add("jem", 8);
+            order.itemsOrdered.Add("jim", 9);
+            order.itemsOrdered.Add("jim", 1);
 
             //assert
-            //testLocations = DbLibrary.ReadAllLocations();
-            //Assert.Equal(expected: DbLibrary.locations.Count, actual: 3);
+            Assert.Equal(expected: 1, actual: order.itemsOrdered["jim"]);
+        }
 
+        [Fact]
+        public void Test12()
+        {
+            //arrange
+            customer.CustID = 1;
+            customer.CustomerFirstName = "Milly";
+            customer.CustomerLastName = "Vanilly";
+
+            //act
+            customers = Mapper.MapCustomer(customer);
+
+            //assert
+            Assert.Equal(expected: "Milly", actual: customers.CustomerFirstName);
         }
     }
 }
